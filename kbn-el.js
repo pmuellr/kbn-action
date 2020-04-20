9dc24ddede545b16a803ea8348ec4710a101bf05
Patrick Mueller <pmuellr@gmail.com>
2020-04-20 01:34:17 -0400

    start filling out some kbn-el function

--- kbn-el.js
+++ kbn-el.js
@@ -16,9 +16,10 @@ const chalk = require('chalk')
 const baseUrl = require('./lib/base-url')
 const commands = require('./lib/el-commands')
 
-const PROGRAM = path.basename(__filename)
-
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
+const PROGRAM = path.basename(__filename)
+const COMMANDS = new Set('all actions action alerts alert es'.split(/\s+/))
 
 // @ts-ignore
 if (require.main === module) main()
@@ -26,51 +27,47 @@ if (require.main === module) main()
 // main cli function
 async function main () {
   const args = parseArgs()
-  const { _, flags } = args
+  const { _, flags, input } = args
 
   if (flags.help) args.showHelp()
   if (flags.version) args.showVersion()
 
-  const urlBase = baseUrl.fromFlags(flags)
-  const command = 'ls'
+  let urlBase = baseUrl.fromFlags(flags)
+  let [command, commandArg] = input
 
-  debugLog(`command: ${command}`)
+  if (flags.es) {
+    urlBase = flags.es
+    command = 'es'
+  }
+
+  if (!command) command = 'all'
+
+  if (command == 'action' && !commandArg) {
+    logError('the action command requires an action id argument')
+  }
+
+  if (command == 'alert' && !commandArg) {
+    logError('the alert command requires an alert id argument')
+  }
+
+  if (!COMMANDS.has(command)) {
+    logError(`invalid command "${command}"`)
+  }
+
+  debugLog(`command: ${command}${commandArg ? ` ${commandArg}` : ''}`)
   debugLog(`flags:   ${JSON.stringify(flags)}`)
 
+  const { startDate, endDate } = getDatesFromFlags(flags.startDate, flags.endDate, flags.duration)
+
   const opts = { debugLog, ...flags }
-  let result
+  opts.startDate = new Date(startDate).toISOString()
+  opts.endDate = new Date(endDate).toISOString()
+
+  let results
   try {
-    result = await commands[command](urlBase, opts)
+    results = await commands[command](urlBase, commandArg, opts)
   } catch (err) {
-    if (err.response == null) {
-      debugLogRequest(err)
-      debugLog(err.stack)
-      logError(err.message)
-    }
-
-    result = err.response
-  }
-
-  debugLogRequest(result)
-
-  debugLog(`http statusCode: ${result.statusCode}`)
-  if (result.statusCode === 302) {
-    const headers = result.headers || {}
-    const redirect = headers.location
-
-    if (redirect.startsWith('/login')) {
-      logError('userid/password required; eg http://elastic:changeme@localhost:5620')
-    }
-
-    logError(`redirected to ${redirect}`)
-  }
-
-  if (result.statusCode === 204) {
-    return
-  }
-
-  if (result.statusCode !== 200) {
-    logError(`status code ${result.statusCode}\nbody: ${JSON.stringify(result.body, null, 4)}`)
+    logError(err.message)
   }
 
   const providerColor = getColoror()
@@ -78,8 +75,8 @@ async function main () {
   const iidColor = getColoror(0)
   const soColor = getColoror(4)
 
-  for (const eventHit of result.body.hits.hits) {
-    const event = eventHit._source
+  for (const eventResult of results) {
+    const event = eventResult._source ? eventResult._source : eventResult
     const { provider, action, duration } = event.event
     const timeStamp = event['@timestamp'].substr(11)
     const instanceId = event.kibana.alerting && event.kibana.alerting.instance_id
@@ -124,21 +121,6 @@ function getSOs(savedObjects) {
   return savedObjects.map(so => `${so.type}:${so.id.substr(0,4)}`)
 }
 
-function debugLogRequest (result) {
-  if (result) {
-    if (result.request) {
-      if (result.request.gotOptions) {
-        const headers = result.request.gotOptions.headers || {}
-        const printedRequestHeaders = new Set(['kbn-xsrf', 'content-type'])
-        for (const name in headers) {
-          if (!printedRequestHeaders.has(name)) continue
-          debugLog(`http request header ${name}: ${headers[name]}`)
-        }
-      }
-    }
-  }
-}
-
 function logError (message) {
   console.log(`${PROGRAM}: ${message}`)
   process.exit(1)
@@ -156,48 +138,136 @@ function getDebugLog () {
 
 // returns parsed args from meow
 function parseArgs () {
-  const defaultUrlBase = process.env.ES_URLBASE || baseUrl.DefaultURL
+  const defaultUrlBase = process.env.KBN_URLBASE || baseUrl.DefaultURL
   const meowOptions = {
     help: getHelpText(),
     flags: {
-      help: { type: 'boolean', alias: 'v' },
+      help: { type: 'boolean', alias: 'h' },
       version: { type: 'boolean', alias: 'v' },
       space: { type: 'string', alias: 's', default: baseUrl.DefaultSpace },
       urlBase: { type: 'string', alias: 'u', default: defaultUrlBase },
-      tags: { type: 'string', default: '' },
-      consumer: { type: 'string', default: 'kbn-alert' },
+      es: { type: 'string' },
+      startDate: { type: 'string', alias: 'b' },
+      endDate: { type: 'string', alias: 'e' },
+      duration: { type: 'string', alias: 'd' },
+      json: { type: 'boolean', alias: 'j' }
     }
   }
 
   // @ts-ignore
-  const meowResponse = meow(meowOptions)
-  if (meowResponse.flags.tags != null) {
-    meowResponse.flags.tags = meowResponse.flags.tags
-      .split(/,/g)
-      .map(tag => tag.trim())
-      .filter(tag => !!tag)
+  return meow(meowOptions)
+}
+
+function getDatesFromFlags(startDateFlag, endDateFlag, durationFlag) {
+  let duration = 1000 * 60 * 60
+  let endDate = Date.now()
+  let startDate = endDate - duration
+
+  startDateFlag = getDateFromString(startDateFlag)
+  endDateFlag = getDateFromString(endDateFlag)
+  durationFlag = getDurationMillis(durationFlag)
+
+  if (startDateFlag && endDateFlag) {
+    startDate = startDateFlag
+    endDate = endDateFlag
   }
 
-  return meowResponse
+  else if (startDateFlag && durationFlag) {
+    startDate = startDateFlag
+    endDate = startDateFlag + durationFlag
+  }
+
+  else if (startDateFlag) {
+    startDate = startDateFlag
+    endDate = Date.now()
+  }
+
+  else if (endDateFlag && durationFlag) {
+    startDate = endDateFlag - durationFlag
+    endDate = endDateFlag
+  }
+
+  else if (endDateFlag) {
+    startDate = endDateFlag - duration
+    endDate = endDateFlag
+  }
+
+  if (startDate > endDate) {
+    logError(`startDate > endDate: ${new Date(startDate)} > ${new Date(endDate)}`)
+  }
+
+  return { startDate, endDate }
+}
+
+/** @type { (dateString: string | null) => number | null } */
+function getDateFromString(dateString) {
+  if (dateString == null) return null
+  dateString = `${dateString}`
+
+  const epochMillis = Date.parse(dateString)
+  if (!isNaN(epochMillis)) {
+    return epochMillis
+  }
+
+  const duration = getDurationMillis(dateString)
+  if (duration != null) {
+    return Date.now() - duration
+  }
+
+  return null
+}
+
+/** @type { (string: null | string) => null | number } */
+function getDurationMillis (string) {
+  if (string == null) return null
+  string = `${string}`
+
+  const match = string.match(/^(\d+)(s|m|h|d)$/)
+  if (match == null) return null
+
+  const units = parseInt(match[1], 10)
+  if (isNaN(units)) return null
+
+  switch(match[2]) {
+    case 's': return units * 1000
+    case 'm': return units * 1000 * 60
+    case 'h': return units * 1000 * 60 * 60
+    case 'd': return units * 1000 * 60 * 60 * 24
+  }
+
+  throw new Error(`unexpected unit ${match[2]}`)
 }
 
 function getHelpText () {
   return `
+write Kibana event log entries to stdout
+
 usage:
   ${PROGRAM}
+  ${PROGRAM} all
+  ${PROGRAM} actions
+  ${PROGRAM} action <action-id>
+  ${PROGRAM} alerts
+  ${PROGRAM} alert <alert-id>
 
 options:
-  -h --help       print this help
-  -v --version    print the version of the program
-  -u --urlBase    Kibana base URL
-  -s --space      Kibana space to use; default: default
+  -h --help               print this help
+  -v --version            print the version of the program
+  -u --urlBase  <url>     Kibana base URL
+  -s --space <space>      Kibana space to use; default: default
+     --es <es-url>        return raw data from elasticsearch
+  -j --json               return data as lines of json strings
 
 You can also set the env var KBN_URLBASE as the Kibana base URL.
 
-Set the DEBUG environment variable to any string for additional diagnostics.
+By default, output is generated as ansi-colored lines with chalk, so you can use
+the environment variable FORCE_COLOR=0 to not generate ansi sequences, or use
+FORCE_COLOR=1 to generate ansi sequences when ordinarily they would not be (eg,
+if you redirect stdout).  The --json option does generate ansi sequences.
 
-The "--tags" option value should be a comma-separated list of tags, defaulting
-to an empty list.
+Using the --es option, all event log documents will be written.
+
+Set the DEBUG environment variable to any string for additional diagnostics.
 
 For authenticated Kibana access, the url should include the userid/password,
 for example "http://elastic:changeme@localhost:5620"
